05356ab7ee416e00715c36d222bfeadd71223bdb
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-05-19 17:22:03 +0200

    update: Change the system of logs

--- config.py
+++ config.py
@@ -6,9 +6,8 @@ load_dotenv()
 DEBUG = True
 HOST = os.getenv('HOST')
 PORT = int(os.getenv('PORT', '8080'))
-
 logging.basicConfig(
-    filename=os.getenv('SERVICE_LOG', 'server.log'),
+    filename=f'{os.path.dirname(__file__)}/logs/app.log',
     level=logging.DEBUG,
     format='%(levelname)s: %(asctime)s pid:%(process)s module:%(module)s %(message)s',
     datefmt='%d/%m/%y %H:%M:%S',
