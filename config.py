8f5327d5ceb18fb14cd1bea4d7d2a90a678686ae
Louis Huort <louis.huort@etudiant.univ-lr.fr>
2022-05-19 18:29:05 +0200

    feat: Add test of log file

--- config.py
+++ config.py
@@ -6,6 +6,11 @@ load_dotenv()
 DEBUG = True
 HOST = os.getenv('HOST')
 PORT = int(os.getenv('PORT', '8080'))
+
+# test if the folder f'{os.path.dirname(__file__)}/logs' exist
+if not os.path.exists(f'{os.path.dirname(__file__)}/logs'):
+    os.mkdir(f'{os.path.dirname(__file__)}/logs')
+
 logging.basicConfig(
     filename=f'{os.path.dirname(__file__)}/logs/app.log',
     level=logging.DEBUG,
